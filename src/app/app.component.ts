f12a01f97c8b507e290c179b5bd17efe408c7eb1
chandra <develpixel@gmail.com>
2017-07-11 13:45:01 +0700

    [data using object] Display - Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  shoppingListItems = [];
+  selectedItem = [];
+
+  clickSelected(item) {
+    console.log(item);
+    return this.selectedItem = item;
+  }
+
+  deleteSelected(item) {
+    console.log(item);
+    if ((item).length !== 0) {
+      this.shoppingListItems.splice(this.shoppingListItems.indexOf(this.selectedItem), 1);
+      this.selectedItem = [];
+    }
+  }
+
+  addItem(itemName, itemAmount) {
+    return this.shoppingListItems.push({ name: itemName.value, amount: itemAmount.value });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FoodComponent } from './food/food.component';
+import { AppRoutingModule } from './modules/app-route.module';
+
+import { FoodListComponent } from './food/food-list/food-list.component';
+import { FoodDetailComponent } from './food/food-detail/food-detail.component';
+import { FoodItemComponent } from './food/food-list/food-item/food-item.component';
+import { FoodService } from './food/food.service';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FoodAddComponent } from './food/food-add/food-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FoodComponent,
+    FoodListComponent,
+    FoodDetailComponent,
+    FoodItemComponent,
+    FoodAddComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [FoodService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
